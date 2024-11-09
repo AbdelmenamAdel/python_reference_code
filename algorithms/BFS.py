23ae5f5e4f020ebcef1f50825f6c59e7dd80f8af
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-09 20:12:12 +0200

    bfs

--- algorithms/BFS.py
+++ algorithms/BFS.py
@@ -1,30 +1,28 @@
 
 # ! ################## Breadth First Search ##################
-
-def BFS(graph, start, end):
-    visited=[]
-    queue = [[start]]
+def BFS(graph,start ,goal):
+    visited =[]
+    queue=[[start]]
     while queue:
-        path = queue.pop(0)
-        node = path[-1]
-
+        path =queue.pop(0)
+        node=path[-1]
         if node in visited:
             continue
-        visited.append(node)
-        if node == end:
+        visited.append(node)  
+        if node is goal:
             return path
-        else :
-            for adjacent in graph.get(node, []):
-                new_path = path.copy()
-                new_path.append(adjacent)
-                queue.append(new_path)
-       
+        else:
+            for adjacent in graph.get(node,[]): # get is a built in function
+                 new_path = path.copy() 
+                 new_path.append(adjacent)
+                 queue.append(new_path)
+
 graph = {
-    'S': ['B','D','A'],
+    'S': ["A", "B","D"],
     "A": ["C"],
     "B": ["D"],
-    "C": ['G','D'],
-    "D": ['G',],
+    "C": ["G","D"],
+    "D": ["G"],
     "g":[]
 }
 path =BFS(graph, "S", "G")
